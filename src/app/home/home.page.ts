da5428cdb78c99eda59feec8b73c34528ee1f8c0
G00408635@atu.ie <G00408635@atu.ie>
2023-03-30 14:46:35 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,40 @@
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Observable, subscribeOn } from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+//import { MovieService } from '../Services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, HttpClientModule, NgFor],
 })
 export class HomePage {
-  constructor() {}
+
+  constructor(private http:HttpClient) {}
+
+  movieData:any[] = [];
+
+  ionViewWillEnter()
+  {
+    this.getMovieData().subscribe((data)=>{
+      this.movieData = data.Search;
+    });
+  }
+
+  getMovieData():Observable<any>
+  {
+    return this.http.get("http://www.omdbapi.com/?apikey=2ee2ed84&s=’war’");
+  }
+
+  // ngOnInit()
+  // {
+  //   this.movie.getMovieData().subscribe((data)=>{
+  //     this.movieData = data.Search;
+  //   });
+  // }
 }
